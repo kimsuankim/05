54d43019c70d7e3d75c0e5ec314956de3faa870c
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2023-10-05 15:48:39 +0900

    2

--- main.c
+++ main.c
@@ -10,14 +10,12 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오  : ");
 	scanf("%d", &x);
 	
-	if(x>0)
-	  printf("양수입니다");
+	if(x>=0) 
+	  printf("절댓값은 %d입니다.",x);
 	  
-    else if(x==0)
-	  printf("0입니다.");
 	  
 	else
-	  printf("음수입니다.");
+	  printf("절댓값은 %d입니다.",-x);
 	
 	return 0;
 }
